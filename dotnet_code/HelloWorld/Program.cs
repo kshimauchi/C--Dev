0d3cce6c30b7f49a8f9836735f91341872742801
unknown <kshimauchi@gmail.com>
2023-05-15 17:26:57 -0700

    First Commit WriteLines

--- dotnet_code/HelloWorld/Program.cs
+++ dotnet_code/HelloWorld/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("C# Programmers!");
+
+// There is no line return just like in Java
+Console.Write("First");
+
+Console.Write("Second");
