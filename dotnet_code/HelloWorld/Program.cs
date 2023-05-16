6dd62f7957938ac7767d4bf7c17635c4f845dec9
unknown <kshimauchi@gmail.com>
2023-05-15 17:37:34 -0700

    program.cs has changed in .net 6

--- dotnet_code/HelloWorld/Program.cs
+++ dotnet_code/HelloWorld/Program.cs
@@ -7,3 +7,20 @@ Console.WriteLine("C# Programmers!");
 Console.Write("First");
 
 Console.Write("Second");
+
+// Oh and yeah the namespace, class and main method has been removed
+
+// using System;
+
+// namespace HelloWorld // Note: actual namespace depends on the project name.
+// {
+//     internal class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello World!");
+//         }
+//     }
+// }
+Console.WriteLine("Break line");
+Console.WriteLine(args[0]); //this will pass whatever in even though we do not see the main method
